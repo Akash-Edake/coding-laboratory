d9287ab08211c8b95ab84019b04d3ba63a97565e
Akash-Edake <akashedake30@gmail.com>
2022-05-29 14:46:49 +0530

    up

---
+++ 5] JS/free-code-camp/11.js
@@ -0,0 +1,7 @@
+//Truthy and Falsy Assignment
+let tool = '';
+tool = 'marker'
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil=tool || 'pen'
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
